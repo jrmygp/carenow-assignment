933affa7391e2fb169b6836fb9c2d03b37cc9b6e
jrmygp <fat.snorlax17@gmail.com>
2024-02-23 14:48:10 +0700

    initial commit

---
+++ server/src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const fs = require("fs");
+
+const app = express();
+dotenv.config();
+
+const PORT = 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>welcome to careNow assignment project</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log("Listening in Port: ", PORT);
+});
