8dc5598fe79cd7002767040f33eea4e5f7a06ccf
jrmygp <fat.snorlax17@gmail.com>
2024-02-25 15:40:55 +0700

    UI : homepage wording changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
     <Grid container spacing={5}>
       <Grid item xs={12} md={6} sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
         <Typography fontWeight={700} component={"h1"} fontSize={30} textAlign="center">
-          Hello welcome to CareNow Treatment Entry System!
+          Hello, welcome to CareNow Treatment Entry System!
         </Typography>
       </Grid>
 
